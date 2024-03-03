ad6d377700d5642d550accfbca37de807f3dd326
SergeyChashnikov <chashserq@gmail.com>
2024-03-03 17:20:00 +0300

    return changes test_main.py

--- test_main.py
+++ test_main.py
@@ -24,10 +24,3 @@ def test_predict_negative():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
-
-def test_predict_negative1():
-    response = client.post("/predict/",
-                           json={"text": "I hate rain!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
